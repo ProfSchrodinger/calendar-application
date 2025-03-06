d60be88e854dcacac87051787f427154284003f6
ProfSchrodinger <sabarimathavan24@gmail.com>
2025-03-06 13:51:06 -0500

    Changed exception

--- src/exception/EventConflictException.java
+++ src/exception/EventConflictException.java
@@ -3,5 +3,6 @@ package exception;
 public class EventConflictException extends RuntimeException {
   public EventConflictException(String message) {
     super(message);
+    System.out.println(message);
   }
 }
--- src/exception/InvalidCommandException.java
+++ src/exception/InvalidCommandException.java
@@ -3,5 +3,6 @@ package exception;
 public class InvalidCommandException extends RuntimeException {
   public InvalidCommandException(String message) {
     super(message);
+    System.out.println(message);
   }
 }
