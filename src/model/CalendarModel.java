f264ca579748aaf10235395600058d257e0fe7c8
ProfSchrodinger <sabarimathavan24@gmail.com>
2025-03-07 20:42:31 -0500

    Base for Calendar created

--- src/model/CalendarModel.java
+++ src/model/CalendarModel.java
@@ -1,4 +1,72 @@
 package model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import exception.EventConflictException;
+
 public class CalendarModel implements ICalendarModel{
+  private List<CalendarEvent> calendar;
+
+  public CalendarModel() {
+    this.calendar = new ArrayList<CalendarEvent>();
+  }
+
+  @Override
+  public CalendarEvent createSingleEvent(CalendarEvent event, boolean autoDecline) throws EventConflictException {
+    for (CalendarEvent existing : calendar) {
+      if (existing.conflictsWith(event) && autoDecline) {
+        throw new EventConflictException("Event Conflict Occured");
+      }
+    }
+    calendar.add(event);
+    return event;
+  }
+
+  @Override
+  public CalendarEvent createRecurringEvent(CalendarEvent event) throws EventConflictException {
+    for (CalendarEvent existing : calendar) {
+      if (existing.conflictsWith(event)) {
+        throw new EventConflictException("Event Conflict Occured");
+      }
+    }
+    calendar.add(event);
+    return event;
+  }
+
+  @Override
+  public void editEvent(String property, String eventName, LocalDateTime startDateTime, LocalDateTime endDateTime, String newValue) throws Exception {
+
+  }
+
+  @Override
+  public void editEvents(String property, String eventName, LocalDateTime startDateTime, String newValue) throws Exception {
+
+  }
+
+  @Override
+  public void editEvents(String property, String eventName, String newValue) throws Exception {
+
+  }
+
+  @Override
+  public List<CalendarEvent> getEventsOn(LocalDateTime date) {
+    return List.of();
+  }
+
+  @Override
+  public List<CalendarEvent> getEventsBetween(LocalDateTime start, LocalDateTime end) {
+    return List.of();
+  }
+
+  @Override
+  public boolean isBusy(LocalDateTime dateTime) {
+    return false;
+  }
+
+  @Override
+  public String exportCalendar(String fileName) throws Exception {
+    return "";
+  }
 }
--- src/model/ICalendarModel.java
+++ src/model/ICalendarModel.java
@@ -1,4 +1,23 @@
 package model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import exception.EventConflictException;
+
 public interface ICalendarModel {
+  CalendarEvent createSingleEvent(CalendarEvent event, boolean autoDecline) throws EventConflictException;
+  CalendarEvent createRecurringEvent(CalendarEvent event) throws EventConflictException;
+
+  void editEvent(String property, String eventName, LocalDateTime startDateTime,
+                 LocalDateTime endDateTime, String newValue) throws Exception;
+  void editEvents(String property, String eventName, LocalDateTime startDateTime, String newValue) throws Exception;
+  void editEvents(String property, String eventName, String newValue) throws Exception;
+
+  List<CalendarEvent> getEventsOn(LocalDateTime date);
+  List<CalendarEvent> getEventsBetween(LocalDateTime start, LocalDateTime end);
+
+  boolean isBusy(LocalDateTime dateTime);
+
+  String exportCalendar(String fileName) throws Exception;
 }
